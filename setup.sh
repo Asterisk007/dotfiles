b6f593c55805da0242e7fec6588fadefe0192408
Asterisk007 <spykid1998@gmail.com>
2022-01-19 12:11:24 -0800

    Create setup file (WIP)

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set up everything for correct dotfile use
+echo "This will overwrite a lot of stuff. You might want to back up your current configuration before continuing."
+read -p "With that in mind, proceed? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+echo "Installing paru"
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+cd ~
+echo "Installing neovim"
+paru -S neovim
+
+
