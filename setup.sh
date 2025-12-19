d2bb3c3c6c7aed017eb40c1edd425407b5a46f27
Daniel Ellingson <spykid1998@gmail.com>
2025-12-19 11:14:38 -0800

    Add more setup commands

--- setup.sh
+++ setup.sh
@@ -8,15 +8,36 @@ then
     exit 1
 fi
 
-if [[ $(which paru) == "" ]]
-then
-    echo "Installing paru"
-    sudo pacman -S --needed base-devel git
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg -si
-fi
+which pacman && {
+    if [ -x $(command -v paru) ]; then
+        echo "Installing paru"
+        sudo pacman -S --needed base-devel git
+        git clone https://aur.archlinux.org/paru.git
+        cd paru
+        makepkg -si
+    fi
+    echo "Installing neovim"
+    paru -S neovim
+}
 
-cd ~
-echo "Installing neovim"
-paru -S neovim
+which apt && {
+    echo "I will need superuser privileges to continue."
+    sudo su
+    echo "Installing neovim and zsh"
+    apt install neovim zsh
+}
+
+echo "Installing oh-my-zsh (plugins and other functionality)"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing ohmyposh (terminal themeing)"
+curl -s https://ohmyposh.dev/install.sh | bash -s
+echo "Installing vim plug for Neovim"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Installing Neovim plugins"
+cp .config ~/
+nvim -c PlugInstall +quitall
+echo "Installing dotfiles"
+cp asterisk.omp.json ~/
+cp .zshrc ~/
+cp .zsh_aliases ~/
